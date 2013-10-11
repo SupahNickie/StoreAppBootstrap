8ad8f0046adc2a7106118ea989bdfb4db49ac431
SupahNickie <nicholascase@live.com>
2013-10-10 18:48:06 -0700

    created store app, used Bootstrap v3 instead of 2.x

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Store::Application.config.secret_key_base = 'ecb4e94b7486bea65ffa6b252d73b92c2ceee6b8ca56cb0dc28f684b803a40acf9966fa7fb80ce89911b581dd9e4f25db234ba880a85f4f5c163bc7e9754792d'
